ca7a26c84d31ac88f3c17a6a162873d3e6915dab
Rakib Hossain <hossainrokib55@gmail.com>
2025-12-27 11:32:41 +0600

    linked_list insert,delete,sort

---
+++ linked_list/insert_at_tail_optimized.cpp
@@ -0,0 +1,58 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    // create a new node
+    Node *newnode = new Node(val);
+
+    // if the list is empty
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    // add new node at the end using tail pointer
+    tail->next = newnode;
+    tail = newnode;
+};
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->next = tail;
+
+    insert_at_tail(head, tail, 100);
+    print_linked_list(head);
+
+    return 0;
+}
