40bf1fd5f1fb24d2e4158ab82b7d2ea2308984b0
Rakib Hossain <hossainrokib55@gmail.com>
2026-01-04 23:44:02 +0600

    insert doubly linked list

---
+++ linked_list/Doubly Linked List/insert_list.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for doubly-linked list node
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+// Insert at head
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    newnode->next = head;
+    head->prev = newnode;
+    head = newnode;
+};
+
+// Insert at tail
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+};
+
+// 
+
+
+// Print linked list forward
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+// print linked list backward
+/* void print_backward(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+} */
+
+int main()
+{
+    
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+
+    a->next = tail;
+    tail->prev = a;
+
+    insert_at_head(head, tail, 100);
+    insert_at_tail(head, tail, 200);
+    print_forward(head);
+    // print_backward(tail);
+
+    return 0;
+}
