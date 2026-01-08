235b4efa2c1e139ce37c6c14adf2c9bd2b1e6935
rakib <hossainrokib55@gmail.com>
2026-01-08 10:13:54 +0600

    detect cycle using single linked list

---
+++ linked_list/Singly Linked List/cycle_list.cpp
@@ -0,0 +1,62 @@
+// Detect cycle implementation (using single linked list)
+// using slow-fast algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+/* // Detect cycle implementation
+void detect_cycle(Node *head)
+{
+} */
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+
+    // 
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = a;
+
+    // Detect cycle implementation
+    Node *slow = head;
+    Node *fast = head;
+    bool flag = false;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag)
+        cout << "Cycle Detected";
+    else
+        cout << "No Cycle";
+
+    return 0;
+}
